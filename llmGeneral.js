2f5901dc2c5b8cd7ecf65e919a804961b1527f75
kazu <u.kazu0604@gmail.com>
2025-06-13 23:05:34 +0900

    agent追加

--- llmGeneral.js
+++ llmGeneral.js
@@ -3,7 +3,7 @@ const clipboardIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height
 <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
 </svg>`
 
-async function sendChatRequest(ask, agenda, system,history = "") {
+async function sendChatRequest(ask, agenda = false, system = "",history = "") {
     var num = 0;
     // 整理できる部分 
     var allQuestion = document.getElementsByName('question')[num].innerHTML = "";
