5950cfac5a75d608ca515da3351a7291912f96a0
kazu <u.kazu0604@gmail.com>
2025-06-15 08:54:18 +0900

    スピーカアイコン

--- llmGeneral.js
+++ llmGeneral.js
@@ -2,8 +2,13 @@ const clipboardIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height
 <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
 <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
 </svg>`
+const speakerIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-volume-up" viewBox="0 0 16 16">
+  <path d="M11.536 14.01a.5.5 0 0 1-.638-.057L7.825 11H5.5A1.5 1.5 0 0 1 4 9.5v-3A1.5 1.5 0 0 1 5.5 5h2.325l3.073-2.953a.5.5 0 0 1 .838.37v11.166a.5.5 0 0 1-.2.4zM8 3.707 5.707 6H5.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h.207L8 12.293V3.707z"/>
+  <path d="M11.5 8a3.5 3.5 0 0 1 0 7 .5.5 0 0 1 0-1 2.5 2.5 0 0 0 0-5 .5.5 0 0 1 0-1z"/>
+</svg>`;
 
-async function sendChatRequest(ask, agenda = false, system = "",history = "") {
+
+async function sendChatRequest(ask, agenda = false, system = "", history = "") {
     var num = 0;
     // 整理できる部分 
     var allQuestion = document.getElementsByName('question')[num].innerHTML = "";
@@ -26,15 +31,15 @@ async function sendChatRequest(ask, agenda = false, system = "",history = "") {
     } else {
         answer.style.display = "";
         var displayAsk = ask;
-        if(history) {
+        if (history) {
             displayAsk += "<br>[返信モード]";
         }
-   
+
         question.innerHTML = marked.parse(displayAsk);
     }
 
     answer.innerHTML = road;
-    var content = await gemini(ask, agenda, system , history);
+    var content = await gemini(ask, agenda, system, history);
     // var content = await llama(ask);
 
     if (agenda) {
@@ -58,14 +63,19 @@ async function sendChatRequest(ask, agenda = false, system = "",history = "") {
             });
         };
 
+        const contentDiv = document.createElement('div');
+        contentDiv.className = 'ans';
+        contentDiv.innerHTML = marked.parse(content);
 
+        const readButton = document.createElement('button');
+        readButton.className = 'btn btn-secondary copy-button';
+        readButton.innerHTML = speakerIcon;
+        readButton.onclick = () => readText('ans');
 
-        answer.innerHTML = `   <button class="btn custom-button" onclick="readText('ans')">読み上げる</button><br>
-<div class="ans">
-${marked.parse(content)}<br>
-</div>`
-        // Append the copy button to the answer element
+        answer.innerHTML = '';
+        answer.appendChild(contentDiv);
         answer.appendChild(copyButton);
+        answer.appendChild(readButton);
     }
 
 
